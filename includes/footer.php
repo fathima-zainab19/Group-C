69df3270baaa3e57929aa82875721f11f922faf6
fathima-zainab19 <yahyazainu2001@gmail.com>
2026-01-03 19:42:32 +0530

    Create footer.php
    includes-footer

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * FOOTER INCLUDE FILE
+ * SHARED FILE - Include at the bottom of dashboard pages
+ */
+?>
+</main>
+</div>
+
+<script src="<?php echo BASE_URL; ?>/public/js/main.js"></script>
+<?php if (isset($extraScripts))
+    echo $extraScripts; ?>
+</body>
+
+</html>
